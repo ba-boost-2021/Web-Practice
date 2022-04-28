e6bc1433148bed74e7a4e33b1d5cb6d3d48ea51c
Sergen Kahraman <sergen_kahraman_@hotmail.com>
2022-04-28 22:21:21 +0300

    card listeleme uygulamasına giriş yapıldı. tasarımlar tamamlandı.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,24 @@
 const template = document.getElementById("cardTemplate");
 const addButton = document.getElementById("add");
-const record = document.getElementById("records");
+const record = document.getElementsByClassName("records")[0];
+const txtFirstName = document.getElementById("txtFirstName");
+const txtLastName = document.getElementById("txtLastName");
+const txtAge = document.getElementById("txtAge");
+const txtDescriptions = document.getElementById("txtDescription");
+function clear() {
+    txtFirstName.value = "";
+    txtLastName.value = "";
+    txtAge.value = "";
+    txtDescriptions.value = "";
+}
 
 addButton.onclick = function () {
-    console.dir(template);
-    template.content.getElementById("firstName").innerHTML = "Berk";
-    template.content.getElementById("lastName").innerHTML = "Özerdoğan";
-    template.content.getElementById("age").innerHTML = "32";
+
+    //console.dir(template);
+    template.content.getElementById("firstName").innerHTML = txtFirstName.value;
+    template.content.getElementById("lastName").innerHTML = txtLastName.value;
+    template.content.getElementById("age").innerHTML = txtAge.value;
+    template.content.getElementById("description").innerHTML = txtDescriptions.value;
     record.innerHTML += template.innerHTML;
+    clear();
 }
